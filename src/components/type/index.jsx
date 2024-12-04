4ef8006c34b32cdf939e35b5262fe8060eef7456
Wylie Fisher <me@wyliefisher.com>
2024-12-03 21:14:16 -0500

    I've royally screwed the themes up, but I'll fix them. Later.

--- src/components/type/index.jsx
+++ src/components/type/index.jsx
@@ -1,51 +1,14 @@
 import React from "react";
-import PropTypes from "prop-types";
 import cn from "classnames";
 
 import { TypeStyled } from "./styled.js";
 
-const Type = ({ children, className, color, ...typeProps }) => {
+const Type = ({ children, className, color = "text", ...typeProps }) => {
   return (
     <TypeStyled className={cn(className)} color={color} {...typeProps}>
       {children}
     </TypeStyled>
   );
-};
-
-Type.displayName = "Type";
-
-Type.propTypes = {
-  /**
-   * Children of the component
-   */
-  children: PropTypes.string,
-  /**
-   * @ignore
-   */
-  className: PropTypes.string,
-  color: PropTypes.string,
-  /**
-   * Is there padding?
-   */
-  padding: PropTypes.bool,
-  /**
-   * Is it the primary font-family
-   */
-  primary: PropTypes.bool,
-  /**
-   * Is it the secondary font-family
-   */
-  secondary: PropTypes.bool,
-  /**
-   * Is it the secondary font-family
-   */
-  $fontSize: PropTypes.string
-};
-
-Type.defaultProps = {
-  children: "",
-  className: "",
-  color: "text"
 };
 
 export default Type;
