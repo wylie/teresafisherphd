3613bb5afa120247810f7253d1b4cf640895ea8e
Wylie Fisher <me@wyliefisher.com>
2024-12-03 21:07:23 -0500

    adding a boilerplate theme.js file

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
+  }
+`;
+export const lightTheme = {
+  body: '#efefef',
+  text: '#333'
+};
+export const darkTheme = {
+  body: '#333',
+  text: '#efefef'
+};
