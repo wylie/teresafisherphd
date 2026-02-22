1353142189e1b2b65fb8fb6bda2b7013fb106d7b
Wylie Fisher <me@wyliefisher.com>
2026-02-22 10:50:26 -0500

    fixing paths for the new URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wylie.github.io',
-  base: 'teresafisherphd',
+  site: 'https://teresafisherphd.com',
   integrations: [partytown(), sitemap()],
   trailingSlash: 'always'
 });
